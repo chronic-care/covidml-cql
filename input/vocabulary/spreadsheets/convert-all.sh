ea9222773b021ecd0827604e2dc69842848f497e
David Carlson <dcarlson@clinicalcloud.solutions>
2024-05-31 10:51:40 -0600

    fix output path

--- input/vocabulary/spreadsheets/convert-all.sh
+++ input/vocabulary/spreadsheets/convert-all.sh
@@ -3,7 +3,7 @@
 # Convet all spreadsheets to FHIR ValueSet resources.
 
 for i in *.xlsx; do
-java -jar ../../../input-cache/tooling-cli-2.4.0.jar -VsacXlsxToValueSet -baseurl=http://emiadvisors.net/fhir/ValueSet/ -pathtospreadsheet=$i -outputpath=../input/vocabulary/ValueSet/xlsx-expanded;
+java -jar ../../../input-cache/tooling-cli-2.4.0.jar -VsacXlsxToValueSet -baseurl=http://emiadvisors.net/fhir/ValueSet/ -pathtospreadsheet=$i -outputpath=../ValueSet/xlsx-expanded;
 done
 
 # This dir hierarchy is added with an empty output folder, maybe due to missing Logger?  Remove this folder.
--- input/vocabulary/valueset-definitions/expand-all.sh
+++ input/vocabulary/valueset-definitions/expand-all.sh
@@ -3,8 +3,5 @@
 # Add expansion to all ValueSet compose definitions.
 
 # for i in *.json; do
-java -jar ../../../input-cache/tooling-cli-2.4.0.jar -EnsureExecutableValueSet -valuesetpath=. -outputpath=../input/vocabulary/ValueSet/expanded;
+java -jar ../../../input-cache/tooling-cli-2.4.0.jar -EnsureExecutableValueSet -valuesetpath=. -outputpath=../ValueSet/expanded;
 # done
-
-# This dir hierarchy is added with an empty output folder, maybe due to missing Logger?  Remove this folder.
-# rm -rf src
